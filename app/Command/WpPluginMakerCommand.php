4a76d5924a5724b39e2eb5251abc57cb7871672c
Anka Bajurin Stiskalov <anka@q-software.com>
2021-10-09 17:51:54 +0200

    copy and rename files with filesystem component, search replace plugin slug, author and version

--- app/Command/WpPluginMakerCommand.php
+++ app/Command/WpPluginMakerCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Service\WPPluginMaker;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -22,12 +23,25 @@ class WpPluginMakerCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this->addArgument('plugin_name', InputArgument::REQUIRED, 'Plugin name');
+        $this->addArgument('author', InputArgument::REQUIRED, 'Author <email>');
+        $this->addArgument('version', InputArgument::OPTIONAL, 'Version in form 1.0.0');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $fileNames = $this->wpPluginMaker->createNewPluginFromWpPluginSkeleton();
+        $plugin_name = $input->getArgument('plugin_name');
+        $author = $input->getArgument('author');
+        $version = $input->getArgument('version') ?: '1.0.0';
+
+        $fileNames = $this->wpPluginMaker->createNewPluginFromWpPluginSkeleton($plugin_name, $author, $version);
         foreach ( $fileNames as  $val ){
             $output->writeln('Writing '.$val);
         }
+        $output->writeln(sprintf('Success! Plugin with name %s, author %s and version %s is created.', $plugin_name, $author, $version ));
+
         return Command::SUCCESS;
     }
 
--- app/Service/WPPluginMaker.php
+++ app/Service/WPPluginMaker.php
@@ -2,29 +2,124 @@
 
 namespace App\Service;
 
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
 class WPPluginMaker
-
 {
-
+    /**
+     * @var Finder
+     */
     protected $finder;
+    /**
+     * @var Filesystem
+     */
+    protected $fileSystem;
 
     public function __construct()
     {
         $this->finder = new Finder();
+        $this->fileSystem = new Filesystem();
     }
 
-    public function createNewPluginFromWpPluginSkeleton(): \Generator
+    public function createNewPluginFromWpPluginSkeleton(string $pluginName, string $author, string $version): \Generator
     {
+        $pluginSlug = $this->slugify($pluginName);
+
+        $newPluginLocation = __DIR__.'/../../'.$pluginSlug.'/';
+
+        try {
+            if (!$this->fileSystem->exists($newPluginLocation))
+            {
+                $this->fileSystem->mkdir($newPluginLocation, 0775);
+            }else{
+                yield "Error creating directory at location ". $newPluginLocation.' Directory already exists.';
+            }
+        } catch (IOExceptionInterface $exception) {
+            yield $exception->getMessage();
+        }
+
         $this->finder->notPath('vendor');
-        $this->finder->files()->in(__DIR__.'/../../wp-plugin-skeleton');
+        $this->finder->files()->in(__DIR__ . '/../../wp-plugin-skeleton');
 
         if ($this->finder->hasResults()) {
+
+            $this->createIgnoredFiles($newPluginLocation);
+
             foreach ($this->finder as $file) {
-                yield $file->getRelativePathname();
+                $fileNameWithExtension = $file->getRelativePathname();
+                $fileContent = $file->getContents();
+                $newFileContent = $this->replaceContent($pluginName, $fileContent, $author);
+
+                if($fileNameWithExtension === 'wp-plugin-skeleton.php'){
+                    $newFileContent = preg_replace('%Version:\s+([0-9.]+[0-9.]+[0-9])%i', 'Version:           '.$version, $newFileContent);
+                    $newFileContent = preg_replace("%define\( 'MY_NEW_PLUGIN_VERSION',\s'([0-9.]+[0-9.]+[0-9])' \)%i", sprintf("define( 'MY_NEW_PLUGIN_VERSION', '%s' )", $version), $newFileContent);
+                }
+
+                $newFileNameWithExtension = str_replace('wp-plugin-skeleton', $pluginSlug, $fileNameWithExtension);
+                $this->fileSystem->dumpFile($newPluginLocation.$newFileNameWithExtension, $newFileContent);
+                yield $newFileNameWithExtension;
             }
         }
 
+    }
+
+    /**
+     * @param string $pluginName
+     * @param string $fileContent
+     * @param string $author
+     * @return string
+     */
+    private function replaceContent(string $pluginName, string $fileContent, string $author): string
+    {
+        $fileContent = str_replace( 'Anka Bajurin Stiskalov', $author, $fileContent);
+        $fileContent = str_replace( 'Wp Plugin Skeleton', $pluginName, $fileContent);
+        $fileContent = str_replace( 'Wp_Plugin_Skeleton', $this->slugify($pluginName, '_', 'ucwords'), $fileContent);
+        $fileContent = str_replace( 'wp_plugin_skeleton', $this->slugify($pluginName, '_'), $fileContent);
+        $fileContent = str_replace( 'wp-plugin-skeleton', $this->slugify($pluginName), $fileContent);
+        return str_replace( 'WP_PLUGIN_SKELETON', $this->slugify($pluginName, '_', 'strtoupper'), $fileContent);
+    }
+
+    /**
+     * Slugify string.
+     *
+     * @param string $text String to slugify.
+     * @param string $replacement
+     * @param string $type
+     * @return string
+     */
+    private function slugify(string $text, string $replacement = '-', string $type = 'strtolower'): string
+    {
+        switch ($type) {
+            case 'ucwords':
+                $text = ucwords($text);
+                break;
+            case 'strtoupper':
+                $text = strtoupper($text);
+                break;
+            default:
+                $text = strtolower($text);
+                break;
+        }
+
+        $text = preg_replace('~[^\\pL\d]+~u', $replacement, $text);
+
+        $text = trim($text, $replacement);
+
+        if (function_exists('iconv')) {
+            $text = iconv('UTF-8', 'ASCII//TRANSLIT', $text);
+        }
+
+        return preg_replace('~[^-\w]+~', '', $text);
+    }
+
+    /**
+     * @param string $newPluginLocation
+     */
+    private function createIgnoredFiles(string $newPluginLocation ): void
+    {
+        $this->fileSystem->dumpFile($newPluginLocation.'.gitignore', '/vendor');
+        $this->fileSystem->dumpFile($newPluginLocation.'.deployignore', '# /vendor');
     }
 }
